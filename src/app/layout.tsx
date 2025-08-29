0cc9414c5e768716df195747d06fad600ed78e6a
BahhTee <85681712+Bakhtiyor7@users.noreply.github.com>
2025-08-29 21:23:52 +0900

    feat: made navbar position fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,50 +1,52 @@
 // src/app/layout.tsx
-import type { Metadata } from "next";
-import "./globals.css";
-import Navbar from "@/components/navbar";
-import HydrationFix from "@/components/HydrationLogger";
-import ThemeRegistry from "@/components/ThemeRegistry";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
-import { Providers } from "@/app/providers";
-import Footer from "@/components/Footer";
-import { dunggeunmo } from "./fonts";
+import type { Metadata } from 'next'
+import './globals.css'
+import Navbar from '@/components/navbar'
+import HydrationFix from '@/components/HydrationLogger'
+import ThemeRegistry from '@/components/ThemeRegistry'
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter'
+import { Providers } from '@/app/providers'
+import Footer from '@/components/Footer'
+import { dunggeunmo } from './fonts'
 
 export const metadata: Metadata = {
-  title: "My Blog",
-  description: "A simple blog with Next.js",
-};
+    title: 'My Blog',
+    description: 'A simple blog with Next.js',
+}
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Press+Start+2P&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className={dunggeunmo.variable} suppressHydrationWarning>
-        <AppRouterCacheProvider>
-          <Providers>
-            <ThemeRegistry>
-              <Navbar />
-              <HydrationFix />
-              <main>{children}</main>
-              <Footer />
-            </ThemeRegistry>
-          </Providers>
-        </AppRouterCacheProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossOrigin=""
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Press+Start+2P&display=swap"
+                    rel="stylesheet"
+                />
+            </head>
+            <body className={dunggeunmo.variable} suppressHydrationWarning>
+                <AppRouterCacheProvider>
+                    <Providers>
+                        <ThemeRegistry>
+                            <Navbar />
+                            <HydrationFix />
+                            <main style={{ paddingTop: '110px' }}>
+                                {children}
+                            </main>
+                            <Footer />
+                        </ThemeRegistry>
+                    </Providers>
+                </AppRouterCacheProvider>
+            </body>
+        </html>
+    )
 }
